b143ec1c5d238374b86d5568829c6088a15277d1
Kelvin <kelvinmah02@gmail.com>
2024-10-12 23:50:07 -0700

    completeed

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geo_url = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+export const geo_options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '28401c7db0msh13944e6f3be85bap166982jsn431e8d7b4d24',
+		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const weather_url = "https://api.openweathermap.org/data/2.5";
+export const weather_key = "5f151c01ccb5417f47d4262d2ed9d628";
+
